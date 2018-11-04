ab0bbd78ee0eb9be64aa4ebeb426627eeecba316
Turcanu Bogdan Alin <bogdanalin92@gmail.com>
2018-11-04 20:45:28 +0200

    Add project files.

---
+++ CSharpInDepth/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharpInDepth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "zero", "one", "two", "three", "four" };
+            int[] numbers = { 0, 1, 2, 3, 4 };
+
+            object[] allStrings = { "These", "are", "all", "strings" };
+            object[] notAllStrings = { "Number", "at", "the", "end", 5 };
+
+            List<User> users = new Users().UsersList;
+            List<string> onlystring = users.SingleOrDefault().Details.ToLower().Split('\\').OfType<string>().ToList();
+            onlystring.ForEach(Console.WriteLine);
+
+            //Creare o colectie enumerabil default ce nu contine nici un element
+            //Initializare
+            var enumInt = Enumerable.Empty<int>();
+
+            //Atribuire o lista de numere intregi
+            enumInt = new List<int>() { 1, 2, 3 };
+
+            //Castarea elementului la o lista de int si afisarea elementelor
+            (enumInt as List<int>).ForEach(Console.WriteLine);
+
+            string[] asString = new string[0];
+
+            int index = asString.Length;
+            foreach (object word in allStrings)
+            {
+                Array.Resize(ref asString, index + 1);
+                asString[index] = (string)word;
+                index++;
+            }
+
+
+            words.GroupJoin(asString,
+                word => word[0],
+                allString => allString[0],
+                (word, matches) => word + ": " + string.Join(";", matches.ToArray())).ToList().ForEach(Console.WriteLine);
+
+            //Procesare Complexa al unui fisier xml cu ajutorul LINQ to XML
+            try
+            {
+                XDocument xdoc = XDocument.Load("xmlDoc.xml");
+                if (xdoc != null)
+                {
+                    var filtered = from p in xdoc.Descendants("Product")
+                                   join s in xdoc.Descendants("Supplier")
+                                   on (int)p.Attribute("SupplierID") equals (int)s.Attribute("SupplierID")
+                                   where (decimal)p.Attribute("Price") > 10
+                                   orderby (string)s.Attribute("Name")
+                                   select new
+                                   {
+                                       SupplierName = (string)s.Attribute("Name"),
+                                       ProductName = (string)p.Attribute("Name")
+                                   };
+
+                }
+            }
+            catch (SystemException sept) { Console.WriteLine(sept.Message); }
+            Console.Read();
+        }
+    }
+}
---
+++ CSharpInDepth/Users.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInDepth
+{
+    class User
+    {
+        public int Id;
+        public string Name;
+        public string Details;
+    }
+    class Users
+    {
+        private readonly List<User> _users;
+        public List<User> UsersList { get { return _users; } }
+        public Users()
+        {
+            _users = new List<User> {
+                new User()
+                {
+                    Id = 0, Name =  "Eu", Details = "Jane Son\\12.41.11\\Labor'm"
+                }
+            };
+        }
+
+    }
+}
